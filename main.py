effadd07522ec1d8a572d90f3ad15a049a611511
Praveensenpai <71966071+Praveensenpai@users.noreply.github.com>
2023-02-26 12:22:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-from functools import partial
+import random
 import string
-from random import choices, shuffle
 
 
-def password_generator(size: int = 8) -> str:
-    letters = [
-        *(
-            string.ascii_lowercase
-            + string.ascii_uppercase
-            + string.digits
-            + string.punctuation
-        )
-    ]
-    password = choices(letters, k=size)
-    shuffle(password)
-    return "".join(password)
+def password_generator(size=8):
+    letters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.sample(letters, size))
+    return password
 
 
 print(password_generator(10))
-password_generator_8 = partial(password_generator, size=8)
-print(password_generator_8())
+print(password_generator(size=8))
